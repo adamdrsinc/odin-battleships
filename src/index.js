2a40ec681096911983ca8bf096198b6f486bfb84
adamdrsinc <adam.sinc124@gmail.com>
2025-09-18 15:26:07 +0100

    feat: add padding and margin #main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./css/style.css";
+import { Player } from "./js/player";
+
+function gameLoop() {
+    const player = new Player("Player");
+    const computer = new Player("Computer");
+
+    
+}
+
+gameLoop();
